4d286af133b0879c1ae4bc4a74d5e6eb95466542
Rik Hoffbauer <Rikhoffbauer@gmail.com>
2015-12-06 23:09:30 +0100

    tunnel icon files

--- server.config.js
+++ server.config.js
@@ -4,6 +4,6 @@
 module.exports = {
   port: 9001,
   serverUrl: 'http://localhost:9000', // should not have a trailing slash
-  extensions: ['gif', 'png', 'jpg', 'js', 'css', 'tag'],
+  extensions: ['gif', 'png', 'jpg', 'js', 'css', 'tag', 'ico'], // extensions to tunnel
   cacheLifeTime: 5 * 60 * 1000 // 5 minutes
 };
